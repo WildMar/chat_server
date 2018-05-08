7e1deb85b88ea70b24c72cd31a05fc00074a3ba3
wildmar <marina.rhodes@gmail.com>
2018-05-08 18:17:40 +0000

    utilize pthreads to handle multiple clients.

--- src/chat_server.c
+++ src/chat_server.c
@@ -9,13 +9,43 @@
 // maintain profiles for clients
 // store history of chat
 
+int process_connection(Server *chat_server)
+{
+    int ret = 0;
+    char buffer[MAX_BUFFER + 1];
+    recv_connection(chat_server);
+    printf("Socket: %d\n", chat_server->csock);
+    if(chat_server->csock == -1)
+    {
+        printf("Exiting\n");
+        close(chat_server->csock);
+        pthread_exit(NULL);
+        return 0;
+    }
+    
+    while((ret = recv_msg(chat_server->csock, buffer)) > 0)
+    {
+        printf("MESSAGE: %s\n", buffer);
+        if(strncmp(buffer, "quit", strlen("quit")) == 0)
+        {
+            close(chat_server->csock);
+            memset(buffer, 0, strlen(buffer));
+            break;
+        }
+        memset(buffer, 0, strlen(buffer));    
+    }
+    printf("Exiting..");
+    close(chat_server->csock);
+    pthread_exit(NULL);
+}
+
 
 int main(void)
 {
-    char buffer[MAX_BUFFER + 1];
+
     int ret = 0;
-//    pthread_t threads[NUM_THREADS];
-//    int rc;
+    pthread_t threads[NUM_THREADS];
+    int rc;
     long t;
     
     Server *chat_server = malloc(sizeof(Server));
@@ -29,49 +59,25 @@ int main(void)
     }
     
     // recv connection and read in message from client
-    while(1)
-    {
-
         // upon receiving a connection, spin off a thread to
         // receive data from client while waiting for
         // more connections
-/*
-        for(t = 0; t < NUM_THREADS; t++)
+
+    for(t = 0; t < NUM_THREADS; t++)
+    {
+        printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, 
+                            process_connection, 
+                            (void *)chat_server);
+        if(rc)
         {
-            printf("In main: creating thread %ld\n", t);
-            rc = pthread_create(&threads[t], NULL, recv_connection, 
-                                (void *)chat_server);
-            if(rc)
-            {
-                printf("ERROR; return code from pthread_create() is %d\n", rc);
-                exit(-1);
-            }
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
         }
-*/
-        recv_connection(chat_server);
-        if(chat_server->lsock == -1)
-        {
-            continue;    
-        }
-        
-        while((ret = recv_msg(chat_server->csock, buffer)) > 0)
-        {
-            printf("MESSAGE: %s\n", buffer);
-            if(strncmp(buffer, "quit", strlen("quit")) == 0)
-            {
-                close(chat_server->csock);
-                memset(buffer, 0, strlen(buffer));
-                break;
-            }
-            memset(buffer, 0, strlen(buffer));    
-        }
-        close(chat_server->csock);
-        
-        //close(csock);
     }
-    
+        //close(csock);
     /* Last thing that main() should do */
-    //pthread_exit(NULL);
+    pthread_exit(NULL);
     free(chat_server);
     return 0;
 }
